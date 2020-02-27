2ee25cf5a7a9cfa656d92610499a08ac8e69e196
J Resilient <j.resilient.1@gmail.com>
2020-02-27 13:24:14 -0500

    Add artworks_controller and necessary actions

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,42 @@
+class ArtworksController < ApplicationController
+    def index
+        render json: Artwork.all
+    end
+
+    def create
+        art = Artwork.new(artwork_params)
+        if art.save
+            render json: art
+        else
+            render json: art.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        art = Artwork.find_by(id: params[:id])
+        art.nil? ? (render json: 'Error: no such artwork.') : (render json: art)
+    end
+
+    def update
+        art = Artwork.find_by(id: params[:id])
+        if art.update(artwork_params)
+            render json: art
+        else
+            render json: art.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        art = Artwork.find_by(id: params[:id])
+        if art.destroy
+            render json: art
+        else
+            render json: 'Error: cannot destroy art'
+        end
+    end
+
+    private
+    def artwork_params
+        params.require(:artwork).permit(:title, :image_url, :artist_id)
+    end
+end
